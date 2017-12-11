c9208de2fc58f202c101b1360b52d3af5e0a06ff
Joel <SchultzJoelJ@hotmail.com>
2017-12-10 20:41:28 -0500

    Implemented sinon testing

--- cow_test.js
+++ cow_test.js
@@ -1,6 +1,22 @@
 var expect = chai.expect;
 
 describe("Cow", function() {
+	var sandbox;
+	
+	beforeEach(function () {
+		// create a sandbox
+		sandbox = sinon.sandbox.create();
+		
+		// stub some console methods
+		sandbox.stub(window.console, "log");
+		sandbox.stub(window.console, "error");
+	});
+	
+	afterEach(function () {
+		// restore the environment as it was before
+		sandbox.restore();
+	});
+	
 	describe("constructor", function() {
 		it("should have a default name", function() {
 			var cow = new Cow();
@@ -14,17 +30,22 @@ describe("Cow", function() {
 	});
 
 	describe("#greets", function() {
-		it("should throw if no target is passed in", function() {
-			expect(function() {
-				(new Cow()).greets();
-				}).to.throw(Error);
-			});
-
-			it("should greet passed target", function() {
-				var greetings = (new Cow("Kate")).greets("Baby");
-				expect(greetings).to.equal("Kate greets Baby");
-			});
+		it("should log and error if no target is passed in", function() {
+			(new Cow()).greets();
+			
+			sinon.assert.notCalled(console.log);
+			sinon.assert.calledOnce(console.error);
+			sinon.assert.calledWithExactly(console.error, "missing target");
 		});
+
+		it("should log greetings", function() {
+			var greetings = (new Cow("Kate")).greets("Baby");
+			
+			sinon.assert.notCalled(console.error);
+			sinon.assert.calledOnce(console.log);
+			sinon.assert.calledWithExactly(console.log, "Kate greets Baby");
+		});
+	});
   
 	describe("#lateGreets", function () {
 		this.slow(1100);
@@ -38,7 +59,9 @@ describe("Cow", function() {
 		
 		it("should greet passed target after one second", function(done) {
 			(new Cow("Kate")).lateGreets("Baby", function(err, greetings) {
-				expect(greetings).to.equal("Kate greets Baby");
+				sinon.assert.notCalled(console.error);
+				sinon.assert.calledOnce(console.log);
+				sinon.assert.calledWithExactly(console.log, "Kate greets Baby");
 				done();
 			});
 		});
