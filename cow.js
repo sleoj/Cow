01c21ac9898f2443025fe5ce75ee7c6b690b55a3
Joel <SchultzJoelJ@hotmail.com>
2017-12-10 19:52:00 -0500

    Initial commit

---
+++ cow.js
@@ -0,0 +1,20 @@
+(function(exports)
+{
+	'use strict';
+	function Cow(name)
+	{
+		this.name = name || "Anon cow";
+	}
+	exports.Cow = Cow;
+
+	Cow.prototype = {
+		greets: function(target)
+		{
+			if (! target)
+			{
+				throw new Error("missing target");
+			}
+			return this.name + " greets " + target;
+		}
+	};
+})(this);
